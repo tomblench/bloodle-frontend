8fbbb29cbaaec0e0d1b876fae45266bedfa7445d
Thomas Blench <tomblench@Thomass-MacBook-Pro.local>
2020-01-31 11:21:00 +0000

    With routing

--- src/App.js
+++ src/App.js
@@ -1,72 +1,60 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import Users from './Users.js';
-import Events from './Events.js';
-import Votes from './Votes.js';
-import VotingSummary from './VotingSummary.js';
-import EventVotes from './EventVotes.js';
-import Choices from './Choices.js';
-import Utils from './Utils';
+import Vote from './Vote.js';
+import Create from './Create.js';
 
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link,
+    useParams
+} from "react-router-dom";
 
 class App extends React.Component {
-  constructor(props) {
-      super(props);
-            this.handleEventChange = this.handleEventChange.bind(this);
-      this.state = {votingSummaryData: {}, votingSummaryIsLoaded: false};
-  }
-
-    fetchData(url, onData, onError) {
-      fetch(url)
-      .then(res => res.json())
-      .then(
-        (result) => {
-            onData(result);
-        },
-        (error) => {
-            onError(error);
-        }
-      );
-    }
-
-    handleEventChange(e){
-        console.log("event changed app");
-        const url = "http://localhost:3000/demo/eventVotes?eventId="+e.target.id;
-        this.fetchData(url,
-                       (result) => {this.setState({votingSummaryData:result, votingSummaryIsLoaded: true});},
-                       (error) => {console.log(error);});        
+    constructor(props) {
+        super(props);
     }
     
     render() {
-            const votingSummaryData = this.state.votingSummaryData;
-            const votingSummaryIsLoaded = this.state.votingSummaryIsLoaded;
         return (
-    <div className="App">
-          <header className="App-header">
-          <h3>Events:</h3>
-          <Events
-      onEventChange={this.handleEventChange}
-      />
-
-          <h3>Voting Summary:</h3>
-                <EventVotes
-            isLoaded={votingSummaryIsLoaded}
-            data={votingSummaryData}
-      />
-
-
-          <h3>Users:</h3>
-          <Users/>
-          <h3>Choices:</h3>
-          <Choices/>
-          <h3>Votes:</h3>
-          <Votes/>
-      </header>
-
-    </div>
-  );
+            <Router>
+              <Switch>
+                <Route path="/vote/:id">
+                  <div className="App">
+                    <header className="App-header">
+                      <h3>Please Vote:</h3>
+                      <VoteRoot/>
+                    </header>
+                  </div>
+                </Route>
+                <Route path="/create">
+                  <div className="App">
+                    <header className="App-header">
+                      <h3>Create A Poll:</h3>
+                      <CreateRoot/>
+                    </header>
+                  </div>
+                </Route>
+              </Switch>
+            </Router>
+        );
     }
+    
 }
+
+function VoteRoot() {
+    let { id } = useParams();
+    return (
+        <Vote eventId={id}/>
+    );
+}
+
+function CreateRoot() {
+    return (
+        <Create/>
+    );
+}
+
 
 export default App;
--- src/Utils.js
+++ src/Utils.js
@@ -2,6 +2,25 @@ class Utils {
     static rewriteLink(l) {
         return l.replace(":8080", ":3000");
     }
+    static fetchData(url, onData, onError, options) {
+        fetch(url, options)
+            .then((response) => {
+                if (!response.ok) {
+                    onError(response.statusText);
+                } else {
+                    return response.json();
+                }
+            })
+            .then(
+                (result) => {
+                    onData(result);
+                },
+                (error) => {
+                    onError(error);
+                }
+            );
+    }
+
 }
 
 export default Utils;
